9890a048f94489300de50bd040a82741ecf78fb8
Rober <rxf_25@hotmail.com>
2021-11-13 18:30:42 +0100

    Revert "Configuración Mybatis"
    This reverts commit 4a94d684201f73b4ea9580a8bc915efadf66a9ef.

--- Prueba/src/main/java/com/prueba/aplicacion/AplicacionApplication.java
+++ Prueba/src/main/java/com/prueba/aplicacion/AplicacionApplication.java
@@ -1,15 +1,13 @@
 package com.prueba.aplicacion;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.prueba.aplicacion.mapper")
 public class AplicacionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AplicacionApplication.class, args);
 	}
-	
+
 }
